71da008c891040f09e702bec96b67ac52f36671e
Kuniwak <orga.chem.job@gmail.com>
2017-10-29 16:44:42 +0900

    Support Carthage

---
+++ MultipartFormData-iOS/MultipartFormData_iOS.h
@@ -0,0 +1,18 @@
+//
+//  MultipartFormData_iOS.h
+//  MultipartFormData-iOS
+//
+//  Created by yuki.kokubun on 2017/10/29.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MultipartFormData_iOS.
+FOUNDATION_EXPORT double MultipartFormData_iOSVersionNumber;
+
+//! Project version string for MultipartFormData_iOS.
+FOUNDATION_EXPORT const unsigned char MultipartFormData_iOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultipartFormData_iOS/PublicHeader.h>
+
+
---
+++ MultipartFormData-watchOS/MultipartFormData_watchOS.h
@@ -0,0 +1,18 @@
+//
+//  MultipartFormData_watchOS.h
+//  MultipartFormData-watchOS
+//
+//  Created by yuki.kokubun on 2017/10/29.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for MultipartFormData_watchOS.
+FOUNDATION_EXPORT double MultipartFormData_watchOSVersionNumber;
+
+//! Project version string for MultipartFormData_watchOS.
+FOUNDATION_EXPORT const unsigned char MultipartFormData_watchOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MultipartFormData_watchOS/PublicHeader.h>
+
+
