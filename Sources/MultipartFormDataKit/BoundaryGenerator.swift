bdf5d6ce17762fc7ddc29312c0542b04d3da2795
dankinsoid <voidilov@gmail.com>
2022-04-29 17:21:02 +0300

    linux support

--- Sources/MultipartFormDataKit/BoundaryGenerator.swift
+++ Sources/MultipartFormDataKit/BoundaryGenerator.swift
@@ -10,6 +10,6 @@ public protocol BoundaryGenerator {
 
 public class RandomBoundaryGenerator: BoundaryGenerator {
     public static func generate() -> String {
-        return String(format: "%08x%08x", arc4random(), arc4random())
+			return String(format: "%08x%08x", UInt32.random(in: 0...UInt32.max), UInt32.random(in: 0...UInt32.max))
     }
 }
